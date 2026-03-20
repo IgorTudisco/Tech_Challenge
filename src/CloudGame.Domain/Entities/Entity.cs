2db9b141bcf81a0c8c565415d312461b411c4c05
Domingos <domingos.neto@globalsys.com.br>
2026-03-19 23:49:31 -0300

    Startup Tech Challenge

---
+++ src/CloudGame.Domain/Entities/Entity.cs
@@ -0,0 +1,21 @@
+﻿namespace CloudGame.Domain.Entities
+{
+    public abstract class Entity<TId>
+    {
+        public TId Id { get; protected set; }
+
+        public DateTime CreatedAt { get; protected set; }
+
+        public DateTime? UpdateAt { get; protected set; }
+
+        public Entity()
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void SetUpdateAt()
+        {
+            UpdateAt = DateTime.UtcNow;
+        }
+    }    
+}
---
+++ src/CloudGame.Test/EmailConstructorTest.cs
@@ -0,0 +1,30 @@
+﻿using CloudGame.Domain.ValueObjects;
+
+namespace CloudGame.Test
+{    
+    public class EmailConstructorTest
+    {
+        [Fact]
+        public void EmailConstructor_ThrowArgumentException_Test()
+        {
+            //arrange
+            string email = "grupo6.com";
+
+            //assert
+            Assert.Throws<ArgumentException>(() => new Email(email));
+        }
+
+        [Fact]
+        public void EmailConstructor_Sucesso_Test()
+        {
+            //arrange
+            string emailString = "grupo6@gmail.com";
+
+            //act
+            Email email =new Email(emailString);
+
+            //assert
+            Assert.True(email.EmailAdressValue.Equals(emailString));
+        }
+    }
+}
