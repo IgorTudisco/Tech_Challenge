962c166328e9080be232077a98ec67b5eaac664e
Guilherme Paschoal <paschoal.gui@gmail.com>
2026-04-12 11:16:41 -0300

    test: should update succesfully

--- src/CloudGame.Test/Handlers/UserHandler/UpdateUserHandlerTest.cs
+++ src/CloudGame.Test/Handlers/UserHandler/UpdateUserHandlerTest.cs
@@ -40,13 +40,17 @@ public class UpdateUserHandlerTest
     [Fact]
     public async Task ShouldBeInvalidWhenUserIsNotFound_Test()
     {
-        var command = new UpdateUserCommand { Id = 1 }; 
+        var command = new UpdateUserCommand { Id = 1 };
 
-        var mock = new Mock<IUserReadOnlyRepository>();
+        var userReadonlyRepositoryMock = new Mock<IUserReadOnlyRepository>();
+        userReadonlyRepositoryMock.Setup(x => x.GetByIdAsync(command.Id)).ReturnsAsync(null as User);
 
-        mock.Setup(x => x.GetByIdAsync(command.Id)).ReturnsAsync(null as User);
+        var userWriteRepositoryMock = new Mock<IUserWriteOnlyRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
 
-        var sut = MakeSut(userReadOnlyRepository: mock.Object);
+        var sut = MakeSut(userReadOnlyRepository: userReadonlyRepositoryMock.Object,
+                    userWriteOnlyRepository: userWriteRepositoryMock.Object,
+                    unitOfWork: unitOfWorkMock.Object);
 
         var result = await sut.HandleAsync(command, CancellationToken.None);
 
@@ -55,5 +59,48 @@ public class UpdateUserHandlerTest
         Assert.Single(result.Errors);
         Assert.Equal(FailedUpdateError.Code, result.Errors.Single().Code);
         Assert.Equal(FailedUpdateError.Description, result.Errors.Single().Description);
+
+        userReadonlyRepositoryMock.Verify(x => x.GetByIdAsync(command.Id), Times.Once);
+        userWriteRepositoryMock.Verify(x => x.UpdateAsync(It.IsAny<User>()), Times.Never);
+        unitOfWorkMock.Verify(x => x.SaveChangesAsync(), Times.Never);
+    }
+
+    [Fact]
+    public async Task ShouldUpdateSuccessfully_Test()
+    {
+        var command = new UpdateUserCommand
+        {
+            Id = 1,
+            BirthDate = DateTime.Now,
+            Email = "validemail@test.com",
+            Name = "Awesome Person"
+        };
+
+
+        var userReadonlyRepositoryMock = new Mock<IUserReadOnlyRepository>();
+        User user = new("oldName", "oldemail@teste.com", "topsecretPassword!!!", DateTime.Now.AddYears(-7), false);
+        userReadonlyRepositoryMock.Setup(x => x.GetByIdAsync(command.Id)).ReturnsAsync(user);
+
+        var userWriteRepositoryMock = new Mock<IUserWriteOnlyRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+        var sut = MakeSut(
+                    userReadOnlyRepository: userReadonlyRepositoryMock.Object,
+                    userWriteOnlyRepository: userWriteRepositoryMock.Object,
+                    unitOfWork: unitOfWorkMock.Object
+                );
+
+        var result = await sut.HandleAsync(command, CancellationToken.None);
+
+        Assert.NotNull(result);
+        Assert.True(result.IsSuccess);
+        Assert.Empty(result.Errors);
+        Assert.Equal(command.Name, user.Name);
+        Assert.Equal(command.Email, user.Email);
+        Assert.Equal(command.BirthDate, user.BirthDate);
+
+        userReadonlyRepositoryMock.Verify(x => x.GetByIdAsync(command.Id), Times.Once);
+        userWriteRepositoryMock.Verify(x => x.UpdateAsync(user), Times.Once);
+        unitOfWorkMock.Verify(x => x.SaveChangesAsync(), Times.Once);
     }
 }
