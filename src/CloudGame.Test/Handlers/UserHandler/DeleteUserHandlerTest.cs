fbd61ffb5a3581c99e35bd5ef7321555a5cee600
Guilherme Paschoal <paschoal.gui@gmail.com>
2026-05-01 20:04:16 -0300

    test: add test method to check if user was deleted and had cleared data

--- src/CloudGame.Test/Handlers/UserHandler/DeleteUserHandlerTest.cs
+++ src/CloudGame.Test/Handlers/UserHandler/DeleteUserHandlerTest.cs
@@ -1,4 +1,5 @@
-﻿using CloudGame.Domain.Commom;
+﻿using CloudGame.Application.Handlers.UserHandler.Delete;
+using CloudGame.Domain.Commom;
 using CloudGame.Domain.Entities;
 using CloudGame.Domain.Interfaces;
 using Moq;
@@ -62,5 +63,38 @@ public class DeleteUserHandlerTest
         userReadonlyRepositoryMock.Verify(x => x.GetByIdAsync(command.Id), Times.Once);
         userWriteRepositoryMock.Verify(x => x.UpdateAsync(It.IsAny<User>()), Times.Never);
         unitOfWorkMock.Verify(x => x.SaveChangesAsync(), Times.Never);
+    }
+
+    [Fact]
+    public async Task ShouldBeDeletedAndUserDataMustBeCleared_Test()
+    {
+        var command = new DeleteUserCommand { Id = 1 };
+
+        var userReadonlyRepositoryMock = new Mock<IUserReadOnlyRepository>();
+
+        var user = new User("John Doe", "john.doe@example", "password123", new DateTime(1990, 1, 1), false);
+
+        userReadonlyRepositoryMock.Setup(x => x.GetByIdAsync(command.Id)).ReturnsAsync(user);
+
+        var userWriteRepositoryMock = new Mock<IUserWriteOnlyRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+        var sut = MakeSut(userReadOnlyRepository: userReadonlyRepositoryMock.Object,
+                    userWriteOnlyRepository: userWriteRepositoryMock.Object,
+                    unitOfWork: unitOfWorkMock.Object);
+
+        var result = await sut.HandleAsync(command, CancellationToken.None);
+
+        Assert.NotNull(result);
+        Assert.True(result.IsSuccess);
+        Assert.Empty(result.Errors);
+        Assert.Equal("@deleted", user.Name);
+        Assert.Equal("@deleted", user.Password);
+        Assert.False(user.Active);
+        Assert.Equal(new DateTime(1950, 1, 1), user.BirthDate);
+
+        userReadonlyRepositoryMock.Verify(x => x.GetByIdAsync(command.Id), Times.Once);
+        userWriteRepositoryMock.Verify(x => x.UpdateAsync(It.IsAny<User>()), Times.Once);
+        unitOfWorkMock.Verify(x => x.SaveChangesAsync(), Times.Once);
     }
 }
