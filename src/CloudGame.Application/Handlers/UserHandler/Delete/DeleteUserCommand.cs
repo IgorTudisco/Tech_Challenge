ab7192d61876ffe9a39ccafc6ed7924b5af835a9
Guilherme Paschoal <paschoal.gui@gmail.com>
2026-05-01 19:58:39 -0300

    test: add implementation class for DeleteUserHandler

---
+++ src/CloudGame.Application/Handlers/UserHandler/Delete/DeleteUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using CloudGame.Domain.Handlers;
+
+namespace CloudGame.Application.Handlers.UserHandler.Delete;
+
+public record DeleteUserCommand : ICommand
+{
+    public int Id { get; init; }
+}
